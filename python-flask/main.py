343e725ce7badbe619fc6796397353b87b5deaa9
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-09-25 16:25:12 +0200

    Store state and nonce in session and verify id_token correctly

--- python-flask/main.py
+++ python-flask/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, session, redirect, make_response
-import os, sys
+from flask import Flask, render_template, request, session, redirect
+import os, sys, time
 import requests
 import jwt
 import uuid
@@ -30,24 +30,28 @@ def sign_in():
   if session.get('signed_in') != None: return redirect('/')
 
   # state is used for CSRF protection. the client generates a value and stores it
-  # for the user somewhere (in a cookie or in a session). it then passes the same value
+  # for the user somewhere (in a cookie or in session). it then passes the same value
   # in the state parameter in the authorize request. IDP will mirror the state value
   # to the redirect URI. the client should then make sure the state value it has stored
   # matches what it receives in the callback
   state = str(uuid.uuid4())
 
-  authorize_url = 'https://stage-id.valtech.com/oauth2/authorize?response_type=%s&client_id=%s&scope=%s&state=%s' % ('code', CLIENT_ID, 'email openid', state)
+  # nonce is introduced in OpenID Connect and serves a similar purpose as state
+  # but for id_token
+  nonce = str(uuid.uuid4())
 
-  resp = make_response(redirect(authorize_url))
-  resp.set_cookie('python-flask-csrf', state)
-  return resp
+  authorize_url = 'https://stage-id.valtech.com/oauth2/authorize?response_type=%s&client_id=%s&scope=%s&state=%s&nonce=%s' % ('code', CLIENT_ID, 'email openid', state, nonce)
+
+  session['state'] = state
+  session['nonce'] = nonce
+  return redirect(authorize_url)
 
 @app.route('/sign-in/callback')
 def sign_in_callback():
   code = request.args.get('code')
   state = request.args.get('state')
 
-  if state != request.cookies.get('python-flask-csrf'):
+  if state != session['state']:
     raise Exception("Possible CSRF detected (state does not match stored state)")
 
   # as both scope openid and email was requested on authorize request above, the client
@@ -61,14 +65,27 @@ def sign_in_callback():
   # as this example app is only interested in who logged in, we will parse the id_token.
   # currently, IDP does not sign id_tokens, but as IDP uses https this is no problem
   # (but the id_token should not be passed around in plaintext where it can be modified by a man-in-the-middle)
-  user_info = jwt.decode(tokens["id_token"], verify=False)
+  id_token = jwt.decode(tokens["id_token"], verify=False)
+
+  # these validations are not strictly necessary in this case as we've just received
+  # the id_token directly from Valtech IDP over https. they are however necessary in
+  # case the id_token was received from an unknown source or transmitted unencrypted
+  if id_token['nonce'] != session['nonce']:
+    raise Exception("Possible CSRF detected (nonce does not match stored nonce)")
+  if id_token['iss'] != 'https://stage-id.valtech.com':
+    raise Exception("id_token not issued by Valtech IDP Stage")
+  if id_token['aud'] != CLIENT_ID:
+    raise Exception("id_token not issued for Valtech IDP Test Client")
+  if int(time.time()) > int(id_token['exp']):
+    raise Exception("id_token has expired")
 
   session['signed_in'] = True
-  session['email'] = user_info['email']
+  #session['email'] = user_info['email']
+  session['email'] = id_token['email']
 
-  resp = make_response(redirect('/'))
-  resp.set_cookie('python-flask-csrf', '', expires=0)
-  return resp
+  session['state'] = None
+  session['nonce'] = None
+  return redirect('/')
 
 @app.route('/sign-out')
 def sign_out():
