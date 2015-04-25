7de35dcf45f0b51eb1e55caa5bb33187f25d2cfb
Emil Wall <emil.wall@valtech.se>
2015-04-25 14:06:42 +0200

    Run test independently so complete name is shown in MS test runner

--- fsharp-mvc/fsharp-mvc-unittest/IdpControllerSpec.fs
+++ fsharp-mvc/fsharp-mvc-unittest/IdpControllerSpec.fs
@@ -6,10 +6,8 @@ open System.Web.Mvc
 
 open fsharp_mvc.Controllers
 
-type ``When signing in``() =
-
-    [<Test>]
-    member this.``Should redirect``() =
-        let controller = new IdpController()
-        let actionResult = controller.SignIn()
-        Assert.False(String.IsNullOrEmpty(actionResult.Url))
+[<Test>]
+let ``When signing in: Should redirect``() =
+    let controller = new IdpController()
+    let actionResult = controller.SignIn()
+    Assert.False(String.IsNullOrEmpty(actionResult.Url))
---
+++ fsharp-mvc/fsharp-mvc/README.md
@@ -0,0 +1,2 @@
+The Resharper test runner does not display test result output (or even name of the tests that are run).
+You need nunit test adapter 2.0 to run the tests with built in test runner.
